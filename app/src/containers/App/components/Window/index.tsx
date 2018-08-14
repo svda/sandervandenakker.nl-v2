8a4964d8b601ad10955353726a6fb9ebe6d3152b
Sander van den Akker <sander@sandervandenakker.nl>
2018-08-14 23:38:09 +0200

    style: updated menu transition

--- app/src/containers/App/components/Window/index.tsx
+++ app/src/containers/App/components/Window/index.tsx
@@ -12,7 +12,7 @@ const styles = {
     'overflow-y': 'auto',
     'box-shadow': 'rgba(0, 0, 0, 0.3) 0 0 10px 3px',
     transform: 'scale(1)',
-    transition: 'all 400ms cubic-bezier(0.175, 0.885, 0.335, 1.05)',
+    transition: 'all 300ms cubic-bezier(0.175, 0.885, 0.335, 1.05)',
     'backface-visibility': 'hidden',
     '-webkit-overflow-scrolling': 'touch',
   }),
