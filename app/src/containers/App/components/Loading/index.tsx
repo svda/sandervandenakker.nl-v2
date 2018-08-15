0fd6449bdef83b968a4290704ee1ba9c75fd5308
Sander van den Akker <sander@sandervandenakker.nl>
2018-08-15 10:37:04 +0200

    feat: Implemented code splitting

---
+++ app/src/containers/App/components/Loading/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import { IThemeProps, masthead } from '../../../../theme';
+import { LoadingComponentProps } from 'react-loadable';
+
+const styles = (theme: any) => ({
+  masthead,
+});
+
+type ILoadingProps = IThemeProps & LoadingComponentProps;
+
+const Loading = ({ classes, isLoading, error, retry }: ILoadingProps) => {
+  if (isLoading) {
+    return <div className={classes.masthead}>Loading...</div>;
+  }
+  else if (error) {
+    return (
+      <div className={classes.masthead}>
+        <Typography variant="display1" align="center" gutterBottom={true}>Sorry, there was a problem loading the page.</Typography>
+        <Button variant="contained" color="secondary" size="large" onClick={retry}>Retry</Button>
+      </div>
+    );
+  }
+  return null;
+}
+
+export default withStyles(styles)(Loading);
--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import * as Loadable from 'react-loadable';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './containers/Home';
-import NotFound from './containers/NotFound';
+import Loading from './containers/App/components/Loading';
 
-const Routes = (props: any) => (
+const Home = Loadable({
+  loader: () => import('./containers/Home'),
+  loading: Loading,
+});
+
+const NotFound = Loadable({
+  loader: () => import('./containers/NotFound'),
+  loading: Loading,
+});
+
+const Routes = (props: React.Props<any>) => (
   <Switch>
     <Route path="/" component={Home} exact={true} />
     <Route path="*" component={NotFound} />
