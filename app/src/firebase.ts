e041678ff88712189e9750083cb841b333e8b0e9
Sander van den Akker <sander@sandervandenakker.nl>
2018-10-03 17:24:29 +0200

    chore: Updated firebase config

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/messaging';
 
 const config = {
-	apiKey: 'AIzaSyCnaRNP0jWD7vgpynO8ExEVhe7lHMv5r28',
-	authDomain: 'svda-24741.firebaseapp.com',
-  databaseURL: 'https://svda-24741.firebaseio.com',
-  messagingSenderId: '735325182068',
-	projectId: 'svda-24741',
-	storageBucket: 'svda-24741.appspot.com',
+  apiKey: 'AIzaSyBUXKL4MjUfLsOVbrjT0_0_InuVAfn3ulY',
+  authDomain: 'svda-firebase.firebaseapp.com',
+  databaseURL: 'https://svda-firebase.firebaseio.com',
+  projectId: 'svda-firebase',
+  storageBucket: 'svda-firebase.appspot.com',
+  messagingSenderId: '827271604881',
 };
 
 firebase.initializeApp(config);
